f270cd0bf2de9afa29b4aa94046e318d952fb29a
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-09-16 19:59:43 +0400

    getpid

--- client.c
+++ client.c
@@ -6,7 +6,20 @@
 /*   By: muganiev <muganiev@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 17:45:18 by muganiev          #+#    #+#             */
-/*   Updated: 2022/09/13 17:45:19 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/09/16 17:09:10 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+ int main()
+ {
+	 while (1)
+	 {
+		 printf("cycle %d\n", getpid());
+		 sleep(1);
+	 }
+	 
+ }
